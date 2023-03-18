7effb142d7fd477738dd561e8e1ae81950fd80e8
Daniil <Comm2k3@hotmail.com>
2023-03-18 04:37:08 +0200

    remove some unnecessary strings

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from random import randint
 
 # Задача №36. Создайте список из случайных чисел. Найдите количество различных элементов в нем.
 
-a = set()
-count = 1
 rand_list = [(int(randint(1, 10))) for _ in range(int(input()))]
 
 print(rand_list)
